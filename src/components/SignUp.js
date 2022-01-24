da0b1f5da5e5fcfa03681a67c0fe31fd226ce79a
yungjhin <63853949+yungjhin@users.noreply.github.com>
2022-01-24 18:21:48 +0000

    sign up work

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,10 +3,25 @@ import { useAuth } from './contexts/AuthContext';
 
 export function SignUp(props) {
 
-    const context = useAuth();
-    console.log(context);
+    const authContext = useAuth();
+
+    function signUp(e) {
+        e.preventDefault();
+
+
+    }
 
     return(
-        "this is the sign up component"
+        <div>
+            <form action="">
+                <label htmlFor="email"> Email </label>
+                <input type="email" name="email"></input>
+                <label htmlFor="password"> Password </label>
+                <input type="password" name="password"></input>
+                <label htmlFor="repeatPassword"> Repeat Password </label>
+                <input type="password" name="repeatPassword"></input>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
     )
 }
