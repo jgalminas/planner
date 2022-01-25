06f1f5896415fccf16d0abd38f24b5f8173d747e
j.galminas <j.galminas@gmail.com>
2022-01-25 20:56:26 +0000

    a

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,25 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useAuth } from './contexts/AuthContext';
 
 export function SignUp(props) {
 
     const authContext = useAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
 
     function signUp(e) {
         e.preventDefault();
+    }
 
-
+    function handleChange(e) {
+        switch (e.target.name) {
+            case 'email':
+                setEmail(e.target.value);
+                break;
+            case 'password':
+                setPassword(e.target.value);
+                break;
+            case 'repeatPassword':
+                setRepeatPassword(e.target.value);
+        }
     }
 
     return(
         <div className="flex row w-100 credentials-form">
-            <form className="flex col gap-10" action="">
+            <form onSubmit={signUp} className="flex col gap-10" action="">
                 <label className="sign-label" htmlFor="email"> Email </label>
-                <input className="sign-input" type="email" name="email"></input>
+                <input value={email} onChange={handleChange} className="sign-input" required type="email" name="email"></input>
                 <label className="sign-label" htmlFor="password"> Password </label>
-                <input className="sign-input" type="password" name="password"></input>
+                <input value={password} onChange={handleChange} className="sign-input" required type="password" name="password"></input>
                 <label className="sign-label" htmlFor="repeatPassword"> Repeat Password </label>
-                <input className="sign-input" type="password" name="repeatPassword"></input>
+                <input value={repeatPassword} onChange={handleChange} className="sign-input" required type="password" name="repeatPassword"></input>
                 <input className="sign-button pointer" type="submit" value="Sign Up"/>
             </form>
         </div>
